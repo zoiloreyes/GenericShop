39dfcad9fb0614a4dedfb278e9fb72c457ef8ba2
Zoilo Ismael Reyes <zreyes@adm.unapec.edu.do>
2019-04-12 11:43:27 -0400

    Arreglado CORS

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -8,8 +8,6 @@ const cors = require('cors')
 const app = express();
 
 
-
-app.use(cors())
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
 
@@ -28,7 +26,13 @@ mongoose.connect(config.DB, {
     console.log('Could not connect to the database. Exiting now...', err);
     process.exit();
 });
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
+  
 // define a simple route
 app.get('/', (req, res) => {
     res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
